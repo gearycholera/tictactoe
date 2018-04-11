dcfff0dfd230914fa996cc2f33992c78d565ae51
girikalra <girikalra@gmail.com>
2018-04-11 14:57:03 -0700

    basic tictactoe functionality

--- tictactoe.js
+++ tictactoe.js
@@ -1,12 +1,14 @@
   var prompt = require('prompt');
 
-  var toggle = true;
-  var turn = 'X';
+  // var toggle = true;
+  // var turn = 'X';
 
-  if (toggle) turn = 'X';
-  else turn = 'O';
+  // if (toggle) turn = 'X';
+  // else turn = 'O';
 
-  var winningCombos = [123,456,789,147,258,369,159,357];
+  var counter = 1;
+
+  var winningCombos = ['012','345', '678', '036', '147', '258', '048', '246'];
 
   var squares = [];
   for (var i = 1; i < 10; i++) {
@@ -14,15 +16,38 @@
     else squares.push(`|_${i}_`);
   }
 
-  var takeTurn = function() {
-    console.log(squares.join(''))
+  var takeTurn = function(turn) {
+    console.log(squares.join(''));
     prompt.start();
 
     prompt.get(['choose'], function (err, result) {
       var choice = +result.choose;
       console.log(turn + ' chose square ' + choice);
-      if (choice-1 % 3 === 0) squares[choice-1] = `|_${turn}_|\n`;
+      if (choice % 3 === 0) squares[choice-1] = `|_${turn}_|\n`;
       else squares[choice-1] = `|_${turn}_`;
-      console.log(squares.join(''))
+      checkForWin(squares, turn);
     });
   }
+
+  var checkForWin = function(board, turn) {
+    for (var i = 0; i < winningCombos.length; i++) {
+      var spots = winningCombos[i].split('');
+      if (squares[spots[0]].split('_')[1] === 'X' 
+          && squares[spots[1]].split('_')[1] === 'X' 
+          && squares[spots[2]].split('_')[1] === 'X') {
+          console.log(squares.join(''));
+          console.log('X is the winner');
+          return;
+      } else if (squares[spots[0]].split('_')[1] === 'O' 
+          && squares[spots[1]].split('_')[1] === 'O' 
+          && squares[spots[2]].split('_')[1] === 'O') {
+          console.log(squares.join(''));
+          console.log('O is the winner');
+          return;
+      }     
+    }
+    if (turn === 'X') takeTurn('O');
+    else takeTurn('X');
+  };
+
+takeTurn('X');
