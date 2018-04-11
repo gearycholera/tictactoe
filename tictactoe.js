921e94b70d2a5cd4863e47b746de1718e8b0dbd6
girikalra <girikalra@gmail.com>
2018-04-11 14:06:29 -0700

    see basic grid in terminal

--- tictactoe.js
+++ tictactoe.js
@@ -1,5 +1,14 @@
   var prompt = require('prompt');
 
+  var squares = [];
+
+  for (var i = 1; i < 10; i++) {
+    if (i % 3 === 0) squares.push(`|_${i}_|\n`);
+    else squares.push(`|_${i}_`);
+  }
+
+  console.log(squares.join(''))
+
   prompt.start();
 
   prompt.get(['name'], function (err, result) {
